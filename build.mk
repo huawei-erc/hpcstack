dfc9c5f6c86c8fa81df4c81d08fdad038fa8f16d
Lauri Leukkunen <lauri.leukkunen@huawei.com>
2014-05-16 16:53:37 +0200

    Cleanups and use rpath
    Signed-off-by: Lauri Leukkunen <lauri.leukkunen@huawei.com>

--- build.mk
+++ build.mk
@@ -3,8 +3,19 @@
 # Author: Lauri Leukkunen
 # License: MIT
 
+ifeq ($(TOPDIR),)
+TOPDIR = $(shell if [ -f ../build.mk ]; then cd ..; pwd; fi)
+endif
+ifeq ($(TOPDIR),)
+TOPDIR = $(shell if [ -f ../../build.mk ]; then cd ../..; pwd; fi)
+endif
+CACHE_DIR = $(TOPDIR)/cache
+
+-include $(TOPDIR)/.hpcstack_local
+
 dirs ?= 
-HPCSTACK_DIR ?= /opt/hpcstack
+PREFIX ?= /opt/hpcstack
+HPCSTACK_DIR ?= $(PREFIX)
 PACKAGE_NAME ?=
 PACKAGE_VER ?=
 DEPENDS ?=
@@ -17,54 +28,27 @@ FETCH ?= wget
 PREFIX ?= $(HPCSTACK_DIR)
 
 PATH := $(HPCSTACK_DIR)/bin:$(PATH)
-LD_LIBRARY_PATH := $(HPCSTACK_DIR)/lib:$(HPCSTACK_DIR)/lib64
+export PATH
 
-export PATH LD_LIBRARY_PATH
+CFLAGS = -I$(HPCSTACK_DIR)/include -I/usr/include
+LDFLAGS = -L$(HPCSTACK_DIR)/lib -Wl,-rpath,$(HPCSTACK_DIR)/lib
+CPPFLAGS = -I$(HPCSTACK_DIR)/include
 
-CFLAGS := -I$(HPCSTACK_DIR)/include -I/usr/include
-LDFLAGS := -L$(HPCSTACK_DIR)/lib -L$(HPCSTACK_DIR)/lib64 -L/usr/lib64 -L/usr/lib -L/lib64 -L/lib
-CPPFLAGS := -I$(HPCSTACK_DIR)/include
-
-ifeq ($(TOPDIR),)
-TOPDIR = $(shell if [ -f ../build.mk ]; then cd ..; pwd; fi)
-endif
-ifeq ($(TOPDIR),)
-TOPDIR = $(shell if [ -f ../../build.mk ]; then cd ../..; pwd; fi)
-endif
-
-CACHE_DIR = $(TOPDIR)/cache
--include $(TOPDIR)/.hpcstack_local
-
-.PHONY: $(dirs)
-
-default: build
-
-all: build
+.DEFAULT_GOAL := build
 
 $(dirs):
 	$(MAKE) -C $@ build
 
-
-%.tar.xz:
-	@mkdir -p $(CACHE_DIR)
-	$(FETCH) $(SOURCE_URL)
-
-%.tar.gz:
-	@mkdir -p $(CACHE_DIR)
-	$(FETCH) $(SOURCE_URL)
-
-%.tar.bz2:
-	@mkdir -p $(CACHE_DIR)
-	$(FETCH) $(SOURCE_URL)
-
 src_full_path := $(foreach f, $(SOURCE_FILE), $(CACHE_DIR)/$(f))
-
 depends_targets = $(addsuffix .depends,$(DEPENDS))
 
-.PHONY: $(depends_targets) depends
+.PHONY: $(depends_targets) depends $(dirs)
+
+$(src_full_path):
+	@mkdir -p $(CACHE_DIR)
+	$(FETCH) $(SOURCE_URL)
 
 $(depends_targets): %.depends:
-# get a lock
 	flock -e $*/.hpcstack_lock -c '$(MAKE) -C $* install'
 
 depends: $(depends_targets)
@@ -75,23 +59,8 @@ depends: $(depends_targets)
 
 .hpcstack_extract: $(src_full_path) .hpcstack_depends
 	@mkdir -p src
-	@(set -e; \
-	if echo $< | grep -q ".tar.gz\$$"; then \
-		tar zxf $< -C src; \
-		exit 0; \
-	fi; \
-	if echo $< | grep -q ".tar.bz2\$$"; then \
-		tar jxf $< -C src; \
-		exit 0; \
-	fi; \
-	if echo $< | grep -q ".tar.xz\$$"; then \
-		tar Jxf $< -C src; \
-		exit 0; \
-	fi; \
-	echo "Unknown file type"; \
-	exit 1;)
+	tar xf $< -C src;
 	@touch .hpcstack_extract
-
 
 .hpcstack_sources: .hpcstack_extract
 	@(set -e; cd $(SRC_DIR); \
@@ -101,23 +70,19 @@ depends: $(depends_targets)
 	done)
 	@touch .hpcstack_sources
 
-
 .hpcstack_autotools_autogen: .hpcstack_sources
 	(set -e; cd $(SRC_DIR); \
 	if [ -x ./autogen.sh ]; then ./autogen.sh; fi;)
 	@touch .hpcstack_autotools_autogen
 
 .hpcstack_autotools_config: .hpcstack_autotools_autogen
-	(set -e; \
-	cd $(SRC_DIR); \
-	./configure CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" --prefix=$(PREFIX) )
+	(set -e; cd $(SRC_DIR); \
+	./configure CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" --prefix=$(PREFIX))
 	@touch .hpcstack_autotools_config
-
 
 .hpcstack_autotools_build: .hpcstack_autotools_config
 	$(MAKE) -C $(SRC_DIR) 
 	@touch .hpcstack_autotools_build
-
 
 .hpcstack_autotools_install: .hpcstack_autotools_build
 	$(MAKE) -C $(SRC_DIR) install
@@ -126,14 +91,12 @@ depends: $(depends_targets)
 .hpcstack_autotools_clean:
 	@rm -rf src/* .hpcstack_*
 
-
 ifeq ("$(BUILD_STYLE)","autotools")
 build: .hpcstack_autotools_build
 
 install: .hpcstack_autotools_install
 
 clean: .hpcstack_autotools_clean
-
 endif
 ifeq ("$(BUILD_STYLE)","custom")
 .hpcstack_custom_build:
@@ -159,7 +122,6 @@ endif
 ##########################################
 
 ifeq ("$(BUILD_STYLE)","category")
-
 build_targets = $(patsubst %,build-%,$(dirs))
 clean_targets = $(patsubst %,clean-%,$(dirs))
 
@@ -172,6 +134,5 @@ $(clean_targets): clean-%:
 build: $(build_targets)
 
 clean: $(clean_targets)
-
 endif
 
